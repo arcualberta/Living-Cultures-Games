43ebda56c023bebe5d060d279d138616ee3066f3
mpm1 <mpm1@ualberta.ca>
2017-04-11 12:14:45 -0600

    Modified to move the Clothing/index.html

--- Clothing/piece-game.js
+++ Clothing/piece-game.js
@@ -12,6 +12,61 @@ var Item = function(x, y, width, height){
 	this.canvas = document.createElement("canvas");
 	this.canvas.width = width;
 	this.canvas.height = height;
+	this.game = null;
+	this.offsetX = -1;
+	this.offsetY = -1;
+	this.div = document.createElement("div");
+
+	this.div.appendChild(this.canvas);
+
+	var div = $(this.div);
+	div.addClass("item");
+	div.focusin(function(event){
+		div.css("resize", "both");
+		event.preventDefault();
+	});
+
+	div.focusout(function(event){
+		div.css("resize", "none");
+		event.preventDefault();
+	});
+
+	//canvas.attr("draggable", true);
+	div.css("width", "100px");
+	div.css("position", "absolute");
+	var _this = this;
+
+	div.mousedown(function(event){
+		var rect = _this.div.getBoundingClientRect();
+		_this.clickStart(event.clientX - rect.left, event.clientY - rect.top);
+	});
+	div.mouseup(function(event){
+		_this.clickEnd();
+	});
+	$(document).mousemove(function(event){
+		_this.clickDrag(event.pageX, event.pageY);
+	});
+};
+Item.prototype.clickStart = function(x, y){
+	if(this.game.selectedItem != null){
+		$(this.game.seletedItem.canvas).focusout();
+	}
+
+	this.offsetX = x;
+	this.offsetY = y;
+
+	this.game.seletedItem = this;
+	//this.div.focus();
+};
+Item.prototype.clickDrag = function(x, y){
+	if(this.offsetX >= 0){
+		$(this.div).css("left", (x - this.offsetX) + "px");
+		$(this.div).css("top", (y - this.offsetY) + "px");
+	}
+};
+Item.prototype.clickEnd = function(){
+	this.offsetX = -1;
+	this.offsetY = -1;
 };
 Item.prototype.update = function(img, pallette){
 	var ctx = this.canvas.getContext("2d");
@@ -41,7 +96,13 @@ var Puzzle = function(imgUrl, componentDimensions, mainDemension){
 	this.loaded = false;
 	this.image = new Image();
 	this.items = [];
-	this.main = mainDemension;
+	this.main = {
+		dim: mainDemension,
+		canvas: document.createElement("canvas")
+	};
+
+	this.main.canvas.width = mainDemension[2];
+	this.main.canvas.height = mainDemension[3];
 
 	this.image.onload = function(){
 		this.loaded = true;
@@ -50,20 +111,32 @@ var Puzzle = function(imgUrl, componentDimensions, mainDemension){
 
 	for(var i = 0; i < componentDimensions.length; ++i){
 		var dim = componentDimensions[i];
-		this.items.push(new Item(dim[0], dim[1], dim[2], dim[3]));
+		this.items.push(new Item(dim[0], dim[1], dim[2], dim[3], game));
 	}
 };
 Puzzle.prototype.update = function(palette){
+	var dim = this.main.dim;
+	var canvas = this.main.canvas;
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(this.image, dim[0], dim[1], dim[2], dim[3], 0, 0, canvas.width, canvas.height);
+
 	for(var i = 0; i < this.items.length; ++i){
 		this.items[i].update(this.image, palette);
 	}
 };
+Puzzle.prototype.setGame = function(game){
+	for(var i = 0; i < this.items.length; ++i){
+		this.items[i].game = game;
+	}
+};
 
-var Game = function(pieceSection, gameSection, puzzles){
+var Game = function(gameSection, puzzles){
 	this.pallette = new Palette();
 	this.puzzles = puzzles;
 	this.current = 0;
-	this.pieceSection = pieceSection;
+	this.gameSection = gameSection;
+	this.selectedItem = null;
 
 	this.setPuzzle(0);
 };
@@ -73,9 +146,26 @@ Game.prototype.setPuzzle = function(index){
 		this.update();
 
 		var puzzle = this.puzzles[index];
-		this.pieceSection.innerHtml = "";
+		puzzle.setGame(this);
+		this.gameSection.innerHtml = "";
+
+		var width = $(document).innerWidth();
+		var height = $(document).innerHeight();
+
+		$(puzzle.main.canvas).css("position", 'absolute');
+		$(puzzle.main.canvas).css("top", '25px');
+		$(puzzle.main.canvas).css("left", (width / 2.0) + 'px');
+		$(puzzle.main.canvas).css("height", (height - 50) + 'px');
+
+		this.gameSection.appendChild(puzzle.main.canvas);
+
 		for(var i = 0; i < puzzle.items.length; ++i){
-			this.pieceSection.appendChild(puzzle.items[i].canvas);
+			var item = $(puzzle.items[i].div);
+			item.css("top", (Math.random() * (height - 60)) + 'px');
+			item.css("left", (Math.random() * (width / 2.0)) + 'px');
+
+			item.attr('tabindex', (i + 1));
+			this.gameSection.appendChild(puzzle.items[i].div);
 		}
 	}
 };
